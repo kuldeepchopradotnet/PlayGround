f0a39006c178a1bdcae96fa4d310afd2922cb6c4
kuldeep <kuldeepchopradotnet@gmail.com>
2019-11-03 14:00:23 +0530

    FormDemo, client side validation, model custom error messge regex etc.

---
+++ PlayGroundMVC/Controllers/FormDemoController.cs
@@ -0,0 +1,31 @@
+﻿using PlayGroundMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlayGroundMVC.Controllers
+{
+    public class FormDemoController : Controller
+    {
+        // GET: FormDemo
+        // Demo to submit a form with razor and apply validation on model property
+        //Server Error in '/' Application.
+        // The current request for action 'Index' on controller type 'FormDemoController' is ambiguous between the following action methods: HttpPost
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormDemoModel formDemoModel)
+        {
+            // is formdemoModel is Valid
+            if (ModelState.IsValid) {
+                ViewBag.Message = "Model is valid";
+            }
+            return View();
+        }
+
+    }
+}
---
+++ PlayGroundMVC/Models/FormDemoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PlayGroundMVC.Models
+{
+    public class FormDemoModel
+    {
+        //https://www.tutorialsteacher.com/mvc/implement-validation-in-asp.net-mvc
+        // apply data annotation attribute like required
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        // Display custom message
+        [Required(ErrorMessage ="Password is required")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,15}$",ErrorMessage ="Not a valid password")]
+        public string Password { get; set; }
+    }
+}
